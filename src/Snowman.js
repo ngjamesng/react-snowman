ce747c386ba8ff73c7e7d35ffce6760015938c63
sairina <sairina.merinotsui@gmail.com>
2020-02-26 23:31:21 -0800

    added condition for winning game, restart button, updated tests, added random word instead of just apple

--- src/Snowman.js
+++ src/Snowman.js
@@ -7,14 +7,20 @@ import img3 from "./3.png";
 import img4 from "./4.png";
 import img5 from "./5.png";
 import img6 from "./6.png";
+import { randomWord, ENGLISH_WORDS } from "./words";
 
 function Snowman({ maxWrong, images, words }) {
   /** by default, allow 6 guesses and use provided gallows images. */
 
   const [nWrong, updateNWrong] = useState(0);
   const [guessed, updateGuessed] = useState(new Set());
-  const [answer, updateAnswer] = useState(words[0]);
+  const [answer, updateAnswer] = useState(randomWord(words));
 
+  function restartGame(){
+    updateNWrong(0);
+    updateGuessed(new Set());
+    updateAnswer(randomWord(words));
+  }
   /** guessedWord: show current-state of word:
     if guessed letters are {a,p,e}, show "app_e" for "apple"
   */
@@ -22,6 +28,11 @@ function Snowman({ maxWrong, images, words }) {
     return answer
       .split("")
       .map(ltr => (guessed.has(ltr) ? ltr : "_"));
+  }
+
+  function win(){
+    const display = guessedWord(); //display is the word after each guess
+    return !display.includes("_"); //returns true if display does not contain _ anymore
   }
 
   /** handleGuest: handle a guessed letter:
@@ -53,13 +64,19 @@ function Snowman({ maxWrong, images, words }) {
     ));
   }
 
+  let output = nWrong < maxWrong ? generateButtons() : `You lose, the word was: ${answer}`;
+  if(win()){
+    output = "You win!";
+  }
+
   /** render: render game */
   return (
     <div className="Snowman" data-testid="snowman">
       <img src={images[nWrong]} alt={`img${nWrong}`} />
       <p> Number wrong: {nWrong}</p>
       <p className="Snowman-word" data-testid="guessedWord">{guessedWord()}</p>
-      <p>{nWrong < maxWrong ? generateButtons() : `You lose`}</p>
+      <p>{output}</p>
+      <button className="Snowman-restart" onClick={restartGame}>Restart</button>
     </div>
   );
 }
@@ -67,7 +84,7 @@ function Snowman({ maxWrong, images, words }) {
 Snowman.defaultProps = {
   maxWrong: 6,
   images: [img0, img1, img2, img3, img4, img5, img6],
-  words: ["apple"]
+  words: ENGLISH_WORDS
 };
 
 export default Snowman;
--- src/Snowman.test.js
+++ src/Snowman.test.js
@@ -2,6 +2,12 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Snowman from "./Snowman";
 
+let testGame;
+const MAX_WRONG = 6;
+
+beforeEach(function() {
+  testGame = render(<Snowman maxWrong={MAX_WRONG} words={["apple"]} />);
+})
 // smoke test
 it("renders without crashing", function () {
   render(<Snowman />);
@@ -9,44 +15,46 @@ it("renders without crashing", function () {
 
 // snapshot test
 it("matches snapshot", function () {
-  const { asFragment } = render(<Snowman />);
-  expect(asFragment()).toMatchSnapshot();
+  // const { asFragment } = render(<Snowman />); // don't need this because of beforeEach
+  expect(testGame.asFragment()).toMatchSnapshot();
 });
 
 it("changes the image to img1 after the first incorrect guess", function () {
-  const { queryByAltText, queryByText } = render(<Snowman />);
-  const wrongLetter = queryByText("m");
-  const img = queryByAltText("img0");
+  const wrongLetter = testGame.queryByText("m");
+  const img = testGame.queryByAltText("img0");
+
   fireEvent.click(wrongLetter);
+
   expect(img).toBeInTheDocument();
   expect(img).toContainHTML(`img1`);
   expect(img).not.toContainHTML(`img0`);
 });
 
 it("the image stays the same and letter is revealed if a correct guess is clicked ", function () {
-  const { queryByAltText, queryByText, getByTestId } = render(<Snowman />);
-  const correctLetter = queryByText("a");
-  const img = queryByAltText("img0");
+  const correctLetter = testGame.queryByText("a");
+  const img = testGame.queryByAltText("img0");
+
   fireEvent.click(correctLetter);
 
   expect(correctLetter).toBeDisabled();
+  
   //checks image 
   expect(img).toContainHTML(`img0`);
   expect(img).not.toContainHTML(`img1`);
+  
   //checks correct letter is revealed 
-  const guessedWord = getByTestId("guessedWord");
+  const guessedWord = testGame.getByTestId("guessedWord");
   expect(guessedWord).toHaveTextContent("a");
   expect(guessedWord).toHaveTextContent("_");
   expect(guessedWord).not.toHaveTextContent("m");
 });
 
 it("should end the game after 6 guesses ", function () {
-  const { queryByText, queryByTestId } = render(<Snowman />);
-  const snowman = queryByTestId("snowman");
+  const snowman = testGame.queryByTestId("snowman");
 
   // Click wrong letters 6 times
   const wrongLetters = [..."bzxqyw"];
-  wrongLetters.forEach(l => fireEvent.click(queryByText(l)));
+  wrongLetters.forEach(l => fireEvent.click(testGame.queryByText(l)));
 
   expect(snowman).toHaveTextContent("You lose");
 });
